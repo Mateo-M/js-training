813dec8c70ae7c09d06ece75d3b019ac219a0c22
Mateo M <me.mateomeana@gmail.com>
2018-04-26 13:51:50 +0200

    finished js tdd p3

--- exercises/get-length.js
+++ exercises/get-length.js
@@ -8,14 +8,17 @@
  */
 
 // Your code :
+function getLength(value) {
+    return value.length;
+}
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.strictEqual(getLength([ 2, 42 ]), 2)
-assert.strictEqual(getLength([ 'pouet', 4, true ]), 3)
+assert.strictEqual(getLength([2, 42]), 2)
+assert.strictEqual(getLength(['pouet', 4, true]), 3)
 assert.strictEqual(getLength(Array(100)), 100)
-assert.strictEqual(getLength([ getLength ]), 1)
+assert.strictEqual(getLength([getLength]), 1)
 assert.strictEqual(getLength('salut'), 5)
 assert.strictEqual(getLength([]), 0)
 // End of tests */
