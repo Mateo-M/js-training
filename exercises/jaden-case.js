8ebad741ada481f9382946d08122293baf422836
Mateo M <me.mateomeana@gmail.com>
2018-04-26 15:01:42 +0200

    finished tdd js p4

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -10,10 +10,22 @@
  *
  */
 
+/*function jadenCase(params) {
+    params.toLowerCase().split(' ').map(function (params) {
+        return (params.charAt(0).toUpperCase())
+    }).join(' ')
+}*/
+function jadenCase(params) {
+    let words = params.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1, words[i].length - 1);
+    }
+    return words.join(" ");
+}
+    //* Begin of tests
+    const assert = require('assert')
 
+    assert.strictEqual(jadenCase('Salut tout le monde'), 'Salut Tout Le Monde')
 
-//* Begin of tests
-const assert = require('assert')
-
-assert.fail('You must write your own tests')
+/*assert.fail('You must write your own tests')*/
 // End of tests */
